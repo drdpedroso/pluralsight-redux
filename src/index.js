4cad1ecb89bfe8c898c036e13e3447b6761b7ac8
Eduardo Pedroso <drdpedroso@gmail.com>
2017-02-26 20:09:42 -0300

    Adding courses route and header

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import './styles/styles.css';
+import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
     <Router history={browserHistory} routes={routes} />,
-    document.getElementById('app');
+    document.getElementById('app')
 );
---
+++ src/styles/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  '#app': {
+    'font': [{ 'unit': 'px', 'value': 14 }, { 'unit': 'string', 'value': ''Helvetica' }, { 'unit': 'string', 'value': 'Neue',' }, { 'unit': 'string', 'value': 'Helvetica,' }, { 'unit': 'string', 'value': 'Arial,' }, { 'unit': 'string', 'value': 'sans-serif' }],
+    'color': '#4d4d4d',
+    'minWidth': [{ 'unit': 'px', 'value': 550 }],
+    'maxWidth': [{ 'unit': 'px', 'value': 850 }],
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }]
+  },
+  'aactive': {
+    'color': 'orange'
+  },
+  'nav': {
+    'paddingTop': [{ 'unit': 'px', 'value': 20 }]
+  }
+});
