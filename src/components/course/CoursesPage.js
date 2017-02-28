a007dbfa4b0515d892a6988a80bff0b5fa212e2c
Eduardo Pedroso <drdpedroso@gmail.com>
2017-02-28 11:46:16 -0300

    Creating course action and reducer

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,14 +6,21 @@ class CoursesPage extends React.Component{
         super(props, context);
 
         this.state = {
-            course: {title: null}
+            course: {title: ''}
         };
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
     }
 
     onTitleChange(event){
         const course = this.state.course;
         course.title = event.target.value;
         this.setState({course: course});
+    }
+
+    onClickSave() {
+        alert(`Saving ${this.state.course.title}`);
     }
 
     render(){
@@ -24,7 +31,7 @@ class CoursesPage extends React.Component{
                 <input type="text" onChange={this.onTitleChange} 
                        value={this.state.course.title} />
 
-                <input type="submit" onChange={this.onClickSave} 
+                <input type="submit" onClick={this.onClickSave} 
                        value="Save" />
             </div>
         );
